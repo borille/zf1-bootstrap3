7f50786946b0c0b2ccdb2b067aa9409be7b195a4
borille <thiagoborille@gmail.com>
2015-06-17 08:33:16 -0300

    Auth model

--- application/models/Auth.php
+++ application/models/Auth.php
@@ -1,42 +1,66 @@
 <?php
 
-class Application_Model_Auth implements Zend_Acl_Role_Interface
+class Application_Model_Auth implements Trb_Acl_Role_Interface
 {
-    /**
-     * @var string
-     */
-    protected $_roleId;
+	/**
+	 * @var string
+	 */
+	protected $_roleId;
 
-    /**
-     * Returns the string identifier of the Role
-     *
-     * @return string
-     */
-    public function getRoleId()
-    {
-        return $this->_roleId;
-    }
+	/**
+	 * @var string
+	 */
+	protected $_login;
 
-    /**
-     * @param $roleId
-     * @return $this
-     */
-    public function setRoleId( $roleId )
-    {
-        $this->_roleId = $roleId;
-        return $this;
-    }
+	/**
+	 * Returns the string identifier of the Role
+	 *
+	 * @return string
+	 */
+	public function getRoleId()
+	{
+		return $this->_roleId;
+	}
 
-    /**
-     * Linguagem
-     * 1 => PT
-     * 2 => EN
-     * @return int
-     */
-    public function getLanguage()
-    {
-        return 1;
-    }
+	/**
+	 * @param $roleId
+	 * @return $this
+	 */
+	public function setRoleId( $roleId )
+	{
+		$this->_roleId = $roleId;
+		return $this;
+	}
+
+	/**
+	 * Linguagem
+	 * 1 => PT
+	 * 2 => EN
+	 * @return int
+	 */
+	public function getLanguage()
+	{
+		return 1;
+	}
+
+	/**
+	 * @param $login
+	 * @return $this
+	 */
+	public function setLogin( $login )
+	{
+		$this->_login = $login;
+		return $this;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getLogin()
+	{
+		return $this->_login;
+	}
+
 
 }
 
